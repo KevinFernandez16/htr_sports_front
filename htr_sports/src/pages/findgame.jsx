ba343723bd31eae5a4062126b9cbb4f002c81628
KevinFernandez16 <kjfernandez1456@gmail.com>
2022-10-12 21:38:38 -0400

    Update findgame.jsx

---
+++ htr_sports/src/pages/findgame.jsx
@@ -0,0 +1,54 @@
+import { MainLayout } from "./mainLayout";
+
+import { useEffect, useState } from "react"; //importing the hooks
+
+const FindGame = () => {
+  const [venue, setVenue] = useState([]);
+
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": '5059162118mshc58ff3e3e67564ap1eea11jsn00e6db6f48d7',
+        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+      },
+    };
+
+      fetch(
+        "https://api-football-v1.p.rapidapi.com/v3/venues?country=Ecuador",
+        options
+      )
+        .then((response) => response.json())
+        // .then(response => console.log(response))
+        .then((data) => {
+          setVenue(data.response)
+          console.log(data);
+        });
+    }
+  , [venue]);
+
+
+  return (
+    <div className="page">
+      <MainLayout>
+        <div>
+
+            {/* <h1> TESTING {venue.name} </h1> */}
+          {venue > 0 &&
+            venue.map((venue) => {
+              return (
+                <div key={venue.id}>
+                  <h1>{venue.address} </h1>
+                  <p> {venue.name} </p>
+                  <p> {venue.id}</p>
+                  <p> {venue.country}</p>
+                </div>
+              );
+            })}
+        </div>
+      </MainLayout>
+    </div>
+  );
+};
+
+export default FindGame;
