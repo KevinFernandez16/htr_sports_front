c9a7ef676830a9f5620978a810c2f02d16208986
KevinFernandez16 <kjfernandez1456@gmail.com>
2022-10-12 22:59:49 -0400

    Update App.js

--- htr_sports/src/App.js
+++ htr_sports/src/App.js
@@ -1,28 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
-//import ReactDOM from 'react-dom'
+import Header from './components/header'
+import Team from './pages/team'
+import Player from './pages/player'
+import Home from './pages/home'
+import Soccer from './pages/soccer'
+import Basketball from './pages/basketball'
+import Fantasy from './pages/fantasy'
+import Forum from './pages/forum'
+import FindGame from './pages/findgame'
+import Paths from './utils/route'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <Router>
+        {/* <Header /> */}
+        <Routes >
+          <Route path={Paths.home.path} element={<Home />} />
+          <Route path={Paths.soccer.path} element={<Soccer />} />
+          <Route path={`${Paths.soccer.path}/team/:id`} element={<Team />} />
+          {/* <Route path={`${Paths.soccer.path}/team/player/:id`} element={<Player />} /> */}
+          <Route path={Paths.basketball.path} element={<Basketball />} />
+          <Route path={Paths.fantasy.path} element={<Fantasy />} />
+          <Route path={Paths.forum.path} element={<Forum />} />
+          <Route path={Paths.findGame.path} element={<FindGame />} />
+          {/* <Route path={Paths[1].path} element={<Soccer />} />
+          <Route path={Paths[4].path} element={<Forum />} /> */}
+        </Routes >
+      </Router>
     </div>
   );
 }
 
 export default App;
-//app
