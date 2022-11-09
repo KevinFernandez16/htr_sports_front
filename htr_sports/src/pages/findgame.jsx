4e8f99a41a20ece058ea1404e16252841802a738
KevinFernandez16 <kjfernandez1456@gmail.com>
2022-11-09 00:16:41 -0500

    Made map appear

--- htr_sports/src/pages/findgame.jsx
+++ htr_sports/src/pages/findgame.jsx
@@ -1,7 +1,8 @@
-import { MainLayout } from "./mainLayout";
+import {useMemo} from "react";
+import {GoogleMap, useLoadScript, Marker} from "@react-google-maps/api";
 import {render} from "react-dom";
 import React, { Component } from "react";
-import { useEffect, useState } from "react"; //importing the hooks
+import "./findgame.css";
 //geolocation begin
 class App extends Component {
   constructor(props){
@@ -29,56 +30,23 @@ class App extends Component {
 render(<App />, document.getElementById("root"));
 //geolocation end
 
-const FindGame = () => {
-  const [venue, setVenue] = useState();
+export default function Home(){
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey:"AIzaSyB-TWLwxfG9pVuLNmDSEp3dA-CW9VHWvBs",
+  });
 
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": '',//import api key
-        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-      },
-    };
+  if(!isLoaded) return <div>Error</div>;
+  return <Map />;
+}
 
-    if (!venue) {
-      fetch(
-        "https://api-football-v1.p.rapidapi.com/v3/venues?country=USA",//grab from api
-        options
-      )
-        .then((response) => response.json())
-        // .then(response => console.log(response))
-        .then((response) => {
-          setVenue(response.response)
-        });
-    }
-  }, [venue]);
-
-//display the api (we got to make sure our code matches with the api's code in order to display specific information)
-  return (
-    <div className="page">
-      <MainLayout>
-        <div>
-
-            {/* <h1> TESTING {venue.name} </h1> */}
-          {venue &&
-            venue.map((venue) => {
-              return (
-                <div key={venue.id}>
-                  <h1>{venue.address} </h1>
-                  <p> {venue.name} </p>
-                  <p> {venue.id}</p>
-                  <p> {venue.city}</p>
-                  <p> {venue.country}</p>
-                  <p> {venue.capacity}</p>
-                  
-                </div>
-              );
-            })}
-        </div>
-      </MainLayout>
+function Map(){
+  return(
+    <div classname = "measure">
+    <GoogleMap
+    zoom = {10}
+    center = {{lat: 44, lng: -80}}
+    mapContainerClassName = "measure"
+    ></GoogleMap>
     </div>
   );
-};
-export default FindGame;
-
+}
