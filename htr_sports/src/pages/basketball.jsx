2dd03b6741502bbcb33bbb2013f7fd77253c57b3
CarlosGenis <carlosgenis06@gmail.com>
2022-11-09 00:07:58 -0500

    Working on getting news to show

--- htr_sports/src/pages/basketball.jsx
+++ htr_sports/src/pages/basketball.jsx
@@ -1,5 +1,41 @@
 import { MainLayout } from "./mainLayout";
 import "../pages/css/basketball.css";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-analytics.js";
+import {getDatabase, set, get, update, remove, ref, child, push, onValue, query} from "https://www.gstatic.com/firebasejs/9.9.4/firebase-database.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut  } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoF3DW_pIDocCRPlrqFpUfxdXtCT7lpFQ",
+  authDomain: "htr-sports.firebaseapp.com",
+  databaseURL: "https://htr-sports-default-rtdb.firebaseio.com",
+  projectId: "htr-sports",
+  storageBucket: "htr-sports.appspot.com",
+  messagingSenderId: "119767757957",
+  appId: "1:119767757957:web:d9f4cfeed391dc8bbde5ec",
+  measurementId: "G-WEB0Z72L02"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getDatabase();
+
+const auth = getAuth(app)
+
+const dbRef = ref(getDatabase(app));
+get(child(dbRef, 'BasketballNews/')).then((snapshot) => {
+    if (snapshot.exists()) {
+      snapshot.forEach(child => {    
+      console.log(child.key, child.val());
+        });
+    } else {
+        console.log("No data available");
+    }
+    }).catch((error) => {
+        console.error(error);
+});
 
 const Basketball = () => {
   return (
@@ -34,6 +70,4 @@ const Basketball = () => {
       </MainLayout>
     </div>
   );
-};
-
-export default Basketball;
+};export default Basketball;
