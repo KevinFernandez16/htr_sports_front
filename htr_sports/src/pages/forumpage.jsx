ec81e9aa0c8d39d4149aa25e5eff0706f2cf0b06
Haley <blahaley@gmail.com>
2022-11-17 02:09:54 -0500

    Change 1
    Added the reply function into the code

--- htr_sports/src/pages/forumpage.jsx
+++ htr_sports/src/pages/forumpage.jsx
@@ -31,6 +31,8 @@ const dbRef = ref(db);
 var editing = false
 
 function editPost(key,title,body){
+    console.log(key);
+    console.log(title);
     if (title == "" || body == ""){
       alert("Both text cannot be empty!");
       return 0
@@ -63,187 +65,64 @@ function deletePost(key){
   });
 }
 
-function showReplies(key){
-  const dbref = ref(db);
-  const repliesArea = document.getElementById("replies");
-  get(child(dbref, '/Posts/'+key+'/Replies'))
-  .then((snapshot)=>{
-    if(snapshot.exists()){
-      var arrayLength = snapshot.val().length;
-      for (var i = 0; i < arrayLength; i++) {
-        const reply_div = document.createElement("div");
-        const Replier = document.createElement("h3");
-        const ReplyPost = document.createElement("p");
-        Replier.innerHTML = snapshot.val()[i].Creator;
-        ReplyPost.innerHTML = snapshot.val()[i].Message;
-
-        reply_div.appendChild(Replier);
-        reply_div.appendChild(ReplyPost);
-
-        repliesArea.appendChild(reply_div);
-        console.log(snapshot.val()[i]);
-      }
-    }
-  });
-}
-
-function replyPost(key,reply){
-  const dbref = ref(db);
-  const user = auth.currentUser;
-  if (user == null){
-    return 0
-  }
-  console.log(user);
-  var replyPos = '0';
-  var numberPos = 0;
-  const replyT = {};
-
-  get(child(dbref, '/Posts/'+key))
-  .then((snapshot)=>{
-    if(snapshot.exists()){
-      replyPos = snapshot.val().ReplyCount
-      console.log(snapshot.val().ReplyCount);
-
-      numberPos = parseInt(replyPos);
-      numberPos += 1;
-      console.log("Next Position: "+numberPos);
-
-      replyT['/Posts/'+key+'/Replies/'+replyPos+"/Message"] = reply;
-      replyT['/Posts/'+key+'/Replies/'+replyPos+"/CreatorID"] = user.uid;
-      get(child(dbref, 'Users/'+user.uid))
-      .then((snapshot)=>{
-        console.log(snapshot.val().DisplayName);
-        replyT['/Posts/'+key+'/Replies/'+replyPos+"/Creator"] = snapshot.val().DisplayName;
-      });
-      replyT['/Posts/'+key+'/ReplyCount'] = numberPos.toString();
-
-      setTimeout(() => {update(dbref,replyT); window.location.reload(); },500);
-    }
-  });
-}
-
-function editPostBtn(){
-  console.log('pressed');
-  if (editing == false){
-    editing = true
-    const area = document.getElementById("editArea");
-
-    const edit_title = document.createElement("input");
-    edit_title.setAttribute('id','edit_title_'+page_id);
-    edit_title.setAttribute('type','text');
-    edit_title.setAttribute('class','edit_title');
-
-    const edit_post = document.createElement("input");
-    edit_post.setAttribute('id','edit_body_'+page_id);
-    edit_post.setAttribute('type','text');
-    edit_post.setAttribute('class','edit_box');
-
-    const commitBtn = document.createElement("button");
-    commitBtn.setAttribute('id','commitBtn');
-    commitBtn.setAttribute('value','Commit');
-    commitBtn.setAttribute('class','button');
-    commitBtn.innerHTML = "Commit";
-
-    area.innerHTML = area.innerHTML + "<br>Title: \t";
-    area.appendChild(edit_title);
-    area.innerHTML = area.innerHTML + "<br>Body: \t";
-    area.appendChild(edit_post);
-    area.innerHTML = area.innerHTML + "<br>";
-    area.appendChild(commitBtn);
-    area.innerHTML = area.innerHTML + "<br>";
-
-    document.getElementById("commitBtn").addEventListener('click',function(){
-      editPost(page_id,document.getElementById('edit_title_'+page_id).value,document.getElementById('edit_body_'+page_id).value)
-    });
-  }else{
-    const area = document.getElementById("editArea");
-    editing = false
-    const element1 = document.getElementById('edit_title_'+page_id);
-    element1.parentNode.removeChild(element1);
-    const element2 = document.getElementById('edit_body_'+page_id);
-    element2.parentNode.removeChild(element2);
-    const element3 = document.getElementById('commitBtn');
-    element3.parentNode.removeChild(element3);
-    area.innerHTML = "";
-  }
-}
-
 async function readData(){
-  const dbref = ref(db);
   const user = auth.currentUser;
-  var username = "";
   console.log("pageid is ",{ page_id });
   const post = document.getElementById("post");
-  const editpost = document.getElementById("editPost");
-  const replypost = document.getElementById("replyArea");
+  const dbref = ref(db);
   get(child(dbref, "Posts/" + page_id))
   .then((snapshot) => {
     if(snapshot.exists()){
       const key = snapshot.val().Owner;
       const PostTitle = snapshot.val().Title;
+      console.log(snapshot.val().Title);
       const PostBody = snapshot.val().Body;
       const PT = document.createElement("h3");
-
-      get(child(dbref, 'Users/'+key))
-      .then((snapshot)=>{
-        PT.innerHTML = snapshot.val().DisplayName + " Posted: " + PostTitle;
-      });
-
+      PT.innerHTML = PostTitle;
       const PB = document.createElement("p");
       PB.innerHTML = PostBody;
       post.appendChild(PT);
       post.appendChild(PB);
-
-      const Breaker = document.createElement("p");
-      Breaker.innerHTML = "--------------------------------------------------------------------";
-
-      post.appendChild(Breaker);
       if (user){
         if (user.uid == key){
-          const edit_Btn = document.createElement("button");
-          edit_Btn.setAttribute('id','editBtn');
-          edit_Btn.setAttribute('value','Edit');
-          edit_Btn.setAttribute('class','button');
-          edit_Btn.innerHTML = "Edit";
+          const edit_title = document.createElement("input");
+          edit_title.setAttribute('id','edit_title_'+page_id);
+          edit_title.setAttribute('type','text');
+          edit_title.setAttribute('class','edit_title');
+          console.log(edit_title.id);
+          const edit_post = document.createElement("input");
+          edit_post.setAttribute('id','edit_body_'+page_id);
+          edit_post.setAttribute('type','text');
+          edit_post.setAttribute('class','edit_box');
+          const editBtn = document.createElement("button");
+          editBtn.setAttribute('id','editBtn');
+          editBtn.setAttribute('value','Edit');
+          editBtn.setAttribute('class','button');
+          editBtn.innerHTML = "Edit";
+          editBtn.addEventListener('click',function(){
+            editPost(page_id,document.getElementById('edit_title_'+page_id).value,document.getElementById('edit_body_'+page_id).value)
+          });
 
           const deleteBtn = document.createElement("button");
           deleteBtn.setAttribute('id','deleteBtn');
           deleteBtn.setAttribute('value','Delete');
           deleteBtn.setAttribute('class','button');
           deleteBtn.innerHTML = "Delete";
-
-          editpost.appendChild(edit_Btn);
-          editpost.innerHTML = editpost.innerHTML + "<br><br>";
-          editpost.appendChild(deleteBtn);
-
-          document.getElementById("editBtn").addEventListener('click',function(){
-            editPostBtn();
-          });
-          document.getElementById("deleteBtn").addEventListener('click',function(){
+          deleteBtn.addEventListener('click',function(){
             deletePost(page_id);
           });
+
+          post.innerHTML = post.innerHTML + "<br>Title: \t";
+          post.appendChild(edit_title);
+          post.innerHTML = post.innerHTML + "<br>Body: \t";
+          post.appendChild(edit_post);
+          post.innerHTML = post.innerHTML + "<br>";
+          //forum.appendChild(edit);
+          post.appendChild(editBtn);
+          post.innerHTML = post.innerHTML + "<br><br>";
+          post.appendChild(deleteBtn);
         }
       }
-      if (user != null){
-        const reply_post = document.createElement("input");
-        reply_post.setAttribute('id','reply_body_'+page_id);
-        reply_post.setAttribute('type','text');
-        reply_post.setAttribute('class','reply_box');
-
-        const reply_Btn = document.createElement("button");
-        reply_Btn.setAttribute('id','replyBtn');
-        reply_Btn.setAttribute('value','Reply');
-        reply_Btn.setAttribute('class','button');
-        reply_Btn.innerHTML = "Reply";
-
-        replypost.appendChild(reply_post);
-        replypost.appendChild(reply_Btn);
-
-        document.getElementById("replyBtn").addEventListener('click',function(){
-          replyPost(page_id,document.getElementById('reply_body_'+page_id).value);
-        });
-      }
-      showReplies(page_id);
     }
   })
 }
@@ -259,18 +138,6 @@ const Forum = () => {
       <MainLayout>
         <div>
           <div id = "post">
-          </div>
-
-          <div id = "editArea">
-          </div>
-
-          <div id = "editPost">
-          </div>
-
-          <div id = "replyArea">
-          </div>
-
-          <div id = "replies"><h3>REPLIES</h3>
           </div>
         </div>
       </MainLayout>
