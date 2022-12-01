83ba25ffbc75f31fae5e229f8d29955073f96bb1
KevinFernandez16 <kjfernandez1456@gmail.com>
2022-11-30 21:08:19 -0500

    Merge branch 'KFfindgame2'

--- htr_sports/src/pages/findgame.jsx
+++ htr_sports/src/pages/findgame.jsx
@@ -1,7 +1,13 @@
+import {useState, useMemo} from "react";
 import { MainLayout } from "./mainLayout";
+import {GoogleMap, useLoadScript, MarkerF} from "@react-google-maps/api";
 import {render} from "react-dom";
 import React, { Component } from "react";
-import { useEffect, useState } from "react"; //importing the hooks
+import "./findgame.css";
+import usePlacesAutocomplete,{getGeocode, getLatLng,}from "use-places-autocomplete";
+import{Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption,} from "@reach/combobox";
+import "@reach/combobox/styles.css";
+
 //geolocation begin
 class App extends Component {
   constructor(props){
@@ -28,57 +34,62 @@ class App extends Component {
 }
 render(<App />, document.getElementById("root"));
 //geolocation end
+export default function Home(){
+  const {isLoaded} = useLoadScript({
+    googleMapsApiKey:"AIzaSyB-TWLwxfG9pVuLNmDSEp3dA-CW9VHWvBs",
+    libraries: ["places"],
+  });
 
-const FindGame = () => {
-  const [venue, setVenue] = useState();
+  if(!isLoaded) return <div>Error</div>;
+  return <Map />;
+}
 
-  useEffect(() => {
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": '',//import api key
-        "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
-      },
-    };
-
-    if (!venue) {
-      fetch(
-        "https://api-football-v1.p.rapidapi.com/v3/venues?country=USA",//grab from api
-        options
-      )
-        .then((response) => response.json())
-        // .then(response => console.log(response))
-        .then((response) => {
-          setVenue(response.response)
-        });
-    }
-  }, [venue]);
-
-//display the api (we got to make sure our code matches with the api's code in order to display specific information)
-  return (
-    <div className="page">
-      <MainLayout>
-        <div>
-
-            {/* <h1> TESTING {venue.name} </h1> */}
-          {venue &&
-            venue.map((venue) => {
-              return (
-                <div key={venue.id}>
-                  <h1>{venue.address} </h1>
-                  <p> {venue.name} </p>
-                  <p> {venue.id}</p>
-                  <p> {venue.city}</p>
-                  <p> {venue.country}</p>
-                  <p> {venue.capacity}</p>
-                  
-                </div>
-              );
-            })}
-        </div>
-      </MainLayout>
+function Map(){
+  const center = useMemo(() => ({lat: 40.7498916, lng: -73.8771786}), []);
+  const [selected, setSelected] = useState(null);
+  return(
+    <>
+    <div className = "places-container">
+      <PlacesAutocomplete setSelected={setSelected}/>
     </div>
+  
+    <GoogleMap
+    zoom = {10}
+    center = {center}
+    mapContainerClassName = "measure"
+    >
+      {selected && <MarkerF position= {selected} />}
+    </GoogleMap>
+    </>
   );
-};
-export default FindGame;
+}
 
+const PlacesAutocomplete = ({setSelected}) => {
+  const{ ready, value, setValue, suggestions: {status, data}, clearSuggestions,} = usePlacesAutocomplete();
+
+  const handleSelect = async(address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    const results = await getGeocode({address});
+    const {lat, lng} = await getLatLng(results[0]);
+    setSelected({lat, lng});
+
+  };
+  return (
+    <MainLayout>
+  <Combobox onSelect ={handleSelect} >
+    <ComboboxInput value = {value} onChange = {event => setValue(event.target.value)} disabled = {!ready}
+    className = "comboinput" placeholder = "Search"
+    />
+    <ComboboxPopover>
+      <ComboboxList>
+        {status == "OK" && data.map(({place_id, description}) => (<ComboboxOption key = {place_id}
+        value = {description} />
+        ))}
+      </ComboboxList>
+    </ComboboxPopover>
+  </Combobox>
+  </MainLayout>
+  );
+}
